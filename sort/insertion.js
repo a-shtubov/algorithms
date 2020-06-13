73c51a60f8e834971fb773047aa43f0436d49bd6
Aleksandr Shtubov <aleksandr.shtubov@yandex.ru>
2020-06-13 23:55:23 +0400

    Add insertion sort with a predicate

--- sort/insertion.js
+++ sort/insertion.js
@@ -18,5 +18,25 @@ function sort(arr) {
   return arr;
 }
 
-const arr1 = Array.from({ length: 10 }, () => Math.round(Math.random() * 20)); //?
-console.log(sort(arr1));
+function sortWith(arr, predicate) {
+  for (let i = 1; i < arr.length; i++) {
+    let el = arr[i];
+    let j = i - 1;
+    let flag = false;
+
+    while (j >= 0 && predicate(arr[j], el) > 0) {
+      arr[j + 1] = arr[j];
+      j--;
+      flag = true;
+    }
+
+    if (flag) {
+      arr[j + 1] = el;
+    }
+  }
+
+  return arr;
+}
+
+// const arr1 = Array.from({ length: 10 }, () => Math.round(Math.random() * 20)); //?
+// console.log(sortWith(arr1, (a, b) => a - b));

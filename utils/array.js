4a36f866d05b83f567af0412f6db415f4d4e67b7
Aleksandr Shtubov <aleksandr.shtubov@yandex.ru>
2020-06-15 23:23:41 +0400

    Add `swap` function

--- utils/array.js
+++ utils/array.js
@@ -3,3 +3,10 @@ export function generateArrayNumber(len, min = 0, max = 50) {
     Math.round(Math.random() * (max - min) + min)
   );
 }
+
+export function swap(arr, i1, i2) {
+  const tmp = arr[i1];
+
+  arr[i1] = arr[i2];
+  arr[i2] = tmp;
+}

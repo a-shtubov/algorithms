e69734d5548e0f12df2c189db867104e84bbffc8
Aleksandr Shtubov <aleksandr.shtubov@yandex.ru>
2020-06-21 23:17:21 +0400

    Add `findBinaryClosest`

--- search/binary.js
+++ search/binary.js
@@ -19,3 +19,38 @@ export function binarySearch(arr, val) {
 
   return res;
 }
+
+export function findBinaryClosest(arr, val, predict) {
+  if (val < arr[0]) {
+    return 0;
+  }
+
+  if (val > arr[arr.length - 1]) {
+    return arr.length - 1;
+  }
+
+  let l = 0;
+  let r = arr.length - 1;
+  let res = -1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (val === arr[mid]) {
+      res = mid;
+      break;
+    }
+
+    if (val < arr[mid]) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+
+  if (res !== -1) {
+    return res;
+  }
+
+  return predict(arr[l], arr[r]) <= 0 ? l : r;
+}

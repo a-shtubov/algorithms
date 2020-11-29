59e04235ca12b497895663b668f5bd911a5acb00
Aleksandr Shtubov <aleksandr.shtubov@yandex.ru>
2020-11-29 22:23:21 +0400

    Add `strstr` solution

---
+++ leetcode/implement-strstr/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+
+  L1: for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      L2: for (let j = 0; j < needle.length; j++) {
+        if (haystack[i + j] !== needle[j]) {
+          continue L1;
+        }
+      }
+
+      return i;
+    }
+  }
+
+  return -1;
+};

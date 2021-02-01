d3ffdde1e604a59bf2ac502faba2a0d7cb18ab02
Aleksandr Shtubov <aleksandr.shtubov@yandex.ru>
2021-02-01 20:48:55 +0400

    Add stack based implementation of `DFS` for `maximum-depth-of-binary-tree` problem

--- leetcode/maximum-depth-of-binary-tree/solution.js
+++ leetcode/maximum-depth-of-binary-tree/solution.js
@@ -20,3 +20,30 @@ var maxDepth = function (root, depth = 0) {
 
   return Math.max(depth, dl, dr);
 };
+
+var maxDepthStack = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  let result = 0;
+  let stack = [[root, 0, 1]];
+
+  while (stack.length > 0) {
+    const [node, flag, depth] = stack.pop();
+
+    if (!node || flag === 2) {
+      continue;
+    }
+
+    result = Math.max(result, depth);
+
+    const nextNode = flag === 0 ? node.left : node.right;
+    const nextFlag = flag + 1;
+
+    stack.push([node, nextFlag, depth]);
+    stack.push([nextNode, 0, depth + 1]);
+  }
+
+  return result;
+};
